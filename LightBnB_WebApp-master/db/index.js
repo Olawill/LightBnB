66d407417c462b16ae07142f4d17b166b2783e96
Olawale <henry.williams658@gmail.com>
2023-09-06 01:04:12 +0000

    Refactor the database pool connection file into index

---
+++ LightBnB_WebApp-master/db/index.js
@@ -0,0 +1,15 @@
+// Node Postgres
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE
+};
+
+const pool = new Pool(config);
+
+
+module.exports = { pool };
